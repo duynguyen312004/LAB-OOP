e0366d7967dc5b10af3aeffc002f3b61999c4d88
Nguyen Khanh Duy <duynguyen312004>
2024-11-25 21:45:36 +0700

    update report

--- AIMSProject/src/hust/soict/dsai/test/store/StoreTest.java
+++ AIMSProject/src/hust/soict/dsai/test/store/StoreTest.java
@@ -1,33 +1,36 @@
-package hust.soict.dsai.test.store;
+// package hust.soict.dsai.test.store;
 
-import hust.soict.dsai.aims.store.Store;
-import hust.soict.dsai.aims.disc.DigitalVideoDisc;
+// import hust.soict.dsai.aims.store.Store;
+// import hust.soict.dsai.aims.disc.DigitalVideoDisc;
 
-public class StoreTest {
-    public static void main(String[] args) {
-        // Tạo một đối tượng cửa hàng
-        Store store = new Store();
+// public class StoreTest {
+// public static void main(String[] args) {
+// // Tạo một đối tượng cửa hàng
+// Store store = new Store();
 
-        // Tạo một số DVD mẫu
-        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95);
-        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 124, 24.95);
-        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladdin", "Animation", "John Musker", 90, 18.99);
+// // Tạo một số DVD mẫu
+// DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation",
+// "Roger Allers", 87, 19.95);
+// DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction",
+// "George Lucas", 124, 24.95);
+// DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladdin", "Animation", "John
+// Musker", 90, 18.99);
 
-        // Thêm DVD vào cửa hàng
-        store.addDVD(dvd1);
-        store.addDVD(dvd2);
-        store.addDVD(dvd3);
+// // Thêm DVD vào cửa hàng
+// store.addDVD(dvd1);
+// store.addDVD(dvd2);
+// store.addDVD(dvd3);
 
-        // Hiển thị danh sách DVD trong cửa hàng
-        store.displayStoreItems();
+// // Hiển thị danh sách DVD trong cửa hàng
+// store.displayStoreItems();
 
-        // Xóa DVD khỏi cửa hàng
-        store.removeDVD(dvd2);
+// // Xóa DVD khỏi cửa hàng
+// store.removeDVD(dvd2);
 
-        // Hiển thị lại danh sách DVD
-        store.displayStoreItems();
+// // Hiển thị lại danh sách DVD
+// store.displayStoreItems();
 
-        // Thử xóa một DVD không có trong cửa hàng
-        store.removeDVD(dvd2);
-    }
-}
+// // Thử xóa một DVD không có trong cửa hàng
+// store.removeDVD(dvd2);
+// }
+// }
